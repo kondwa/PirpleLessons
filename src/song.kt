1dd129bc96ccabf7c97df140182e5264c6575534
Kondwani Hara <kondwa@gmail.com>
2020-09-17 20:32:41 +0200

    added comments for extra credit.

--- src/song.kt
+++ src/song.kt
@@ -1,10 +1,11 @@
 fun main(){
+    // set the song attributes
     val title:String = "Voice of Truth"
     val artist:String = "Casting Crowns"
     val album:String = "Casting Crowns"
     val yearReleased: Int = 2003
     val genre:String = "Religious"
     val duration:String = "5:25"
-
+    // display the song attributes
     println(" Song title: $title\n Artist: $artist\n Album: $album\n Genre: $genre\n Year Released: $yearReleased\n Duration: $duration")
 }
