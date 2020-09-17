ea68637b15caea58e215ca86a10ea8ed1e6d388d
Kondwani Hara <kondwa@gmail.com>
2020-09-17 20:01:41 +0200

    finished the kotlin variable assignment

---
+++ src/song.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val title:String = "Voice of Truth"
+    val artist:String = "Casting Crowns"
+    val album:String = "Casting Crowns"
+    val yearReleased: Int = 2003
+    val genre:String = "Religious"
+    val duration:String = "5:25"
+
+    println(" Song title: $title\n Artist: $artist\n Album: $album\n Genre: $genre\n Year Released: $yearReleased\n Duration: $duration")
+}
